d6ff5036744cb3d3654fc47c3be200bd59aedd4d
Emre YILMAZ <emreyilmaz@yandex.com>
2020-11-30 00:27:46 +0300

    nixpkgs version pinned

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{pkgs  ? import <nixpkgs> {}}:
+{pkgs  ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2335e7354f8a9c34d4842aa107af14e268a27f07.tar.gz") {}}:
   pkgs.haskellPackages.callPackage ./bahriye.nix {}
